7b831bd2a416384f6a913a1cc791183c9fcd50fa
uykb <tuzhenqiang@gmail.com>
2026-03-10 17:47:27 +0800

    fix: disable maxRetriesPerRequest for Redis to prevent startup crash

--- src/utils/redis.js
+++ src/utils/redis.js
@@ -8,9 +8,19 @@ const redis = new Redis({
   host: redisConfig.host,
   port: redisConfig.port,
   password: redisConfig.password,
+  maxRetriesPerRequest: null, // Disable the max retries limit to allow infinite retries during startup
   retryStrategy: (times) => {
+    // Linear backoff: 50ms, 100ms, 150ms... up to 2000ms
     const delay = Math.min(times * 50, 2000);
     return delay;
+  },
+  reconnectOnError: (err) => {
+    const targetError = 'READONLY';
+    if (err.message.includes(targetError)) {
+      // Only reconnect when the error contains "READONLY"
+      return true;
+    }
+    return false;
   }
 });
 
